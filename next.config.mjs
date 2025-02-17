9602feb05286846629c33403d4bdbe6077e2439b
Monu Rajj <monu2feb2004.com>
2025-02-17 10:23:26 +0530

    redireced to /Dashboard on launching

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/DashBoard",
+        permanent: true, // Change to false if you want a temporary redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
